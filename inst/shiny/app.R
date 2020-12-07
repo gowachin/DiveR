22e5b46848a13a8646ef79b56bc4be35a1ace836
gowachin <gowachin974@gmail.com>
2020-12-07 19:00:33 +0100

    first step in css

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -4,12 +4,16 @@ library(mn90)
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
+  tags$head(
+    tags$link(rel = 'stylesheet', type = 'text/css', href = 'bootstrap.css')
+  ),
+  
   # App title ----
   titlePanel("MN90"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(position = "right",
-    
+
     # Sidebar panel for inputs ----
     sidebarPanel(
       titlePanel("First dive"),
@@ -19,7 +23,7 @@ ui <- fluidPage(
                   min = 6,
                   max = 65,
                   value = 20),
-      
+
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "time1",
                   label = "Time (minutes):",
@@ -27,18 +31,18 @@ ui <- fluidPage(
                   max = 180,
                   value = 40),
       checkboxInput("secu1", "Security stop", TRUE),
-      
+
       checkboxInput("sec", "Second Dive"),
-      
+
       conditionalPanel(
         condition = "input.sec == true",
-        
+
         sliderInput(inputId = "depth2",
                     label = "Depth (meter):",
                     min = 6,
                     max = 65,
                     value = 20),
-        
+
         sliderInput(inputId = "time2",
                     label = "Time (minutes):",
                     min = 1,
@@ -46,21 +50,21 @@ ui <- fluidPage(
                     value = 40)
       )
     ),
-    
+
     # Main panel for displaying outputs ----
     mainPanel(
       h2('This is a dive with a square profile and stops given from tables (MN90 tables from the FFESSM.'),
-      
+
       textOutput("dive1"),
       # Output: Histogram ----
       plotOutput(outputId = "divePlot"),
-      
+
       conditionalPanel(
         condition = "input.sec == true",
-        
+
         textOutput("dive2")
       )
-      
+
     )
   )
 )
@@ -72,17 +76,17 @@ server <- function(input, output, session) {
     updateSliderInput(session, "time1", value = 1,
                       min = 0, max = maxt1)
   })
-  
+
   output$divePlot <- renderPlot({
     dive <- dive(depth = input$depth1, time = input$time1, secu = input$secu1, vup = 10)
     plot(dive)
   })
-  
-  output$dive1 <- renderText({ 
+
+  output$dive1 <- renderText({
     paste("You have selected", input$depth1, 'for', input$time1)
   })
-  
-  output$dive2 <- renderText({ 
+
+  output$dive2 <- renderText({
     paste("You have selected", input$depth2, 'for', input$time2)
   })
 }
